7be8129ec20ed05382594003616e2e731d6dfb5d
Irfan <irfantayyib07@gmail.com>
2023-11-05 14:49:12 +0500

    video8

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,10 +1,14 @@
 import { useState } from "react";
 
 function TextForm(props) {
- const [text, setText] = useState("Enter text here");
+ const [text, setText] = useState("");
 
- function handleUppercase() {
+ function handleUpperCase() {
   setText(text.toUpperCase());
+ }
+
+ function handleLowerCase() {
+  setText(text.toLowerCase());
  }
 
  function handleOnChange(event) {
@@ -12,14 +16,24 @@ function TextForm(props) {
  }
 
  return (
+  <>
   <div className="container my-3">
    <div className="mb-3">
     <label htmlFor="myBox" className="form-label"><h4>{props.heading}</h4></label>
     <textarea className="form-control" value={text} id="myBox" rows="8" onChange={handleOnChange}></textarea>
    </div>
-
-   <button className="btn btn-dark" onClick={handleUppercase}>UPPERCASE</button>
+   <button className="btn btn-dark" onClick={handleUpperCase}>UPPERCASE</button>
+   <button className="btn btn-dark mx-2" onClick={handleLowerCase}>lowercase</button>
   </div>
+
+  <div className="container">
+   <h4>Your Text Summary</h4>
+   <p>{text.split(" ").length} words and {text.length} characters</p>
+   <p>{0.008 * text.split(" ").length} minutes read</p>
+   <h4>Preview</h4>
+   <p>{text}</p>
+  </div>
+  </>
  )
 }
 
